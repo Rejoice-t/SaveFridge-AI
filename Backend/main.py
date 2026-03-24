17fab924c0893f74e6aa0217d7c1e5342168b826
Rejoice-t <rejoice.teca@gmail.com>
2026-03-24 16:37:13 +0200

    finalize SaveFridge AI with frontend, backend, and README

--- Backend/main.py
+++ Backend/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
 from models import IngredientRequest
 from ai import generate_recipes
+from fastapi.middleware.cors import CORSMiddleware
 
 # Create the FastAPI application
 app = FastAPI()
+## CORS middleware to allow requests from the frontend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Home endpoint to test if the server is running
 @app.get("/")
